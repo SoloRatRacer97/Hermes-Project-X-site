471f290097d26965347a3c230ce6437d16a22e1b
Todd Anderson <toddanderson@Gaius-Mainframe.local>
2026-04-20 10:04:12 -0700

    chore: publish landing for Captain Decks

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -162,7 +162,7 @@ export const siteConfig = {
   buildMeta: {
     clientId: "captain-decks",
     companyName: "Captain Decks",
-    generatedAt: "2026-04-20T17:03:00.558Z",
+    generatedAt: "2026-04-20T17:04:07.873Z",
     profileSchemaDate: "2026-04-20",
   } as BuildMeta,
 };
